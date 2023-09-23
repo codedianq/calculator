2af01c6d83385eb632b4669d26704a2dcc578848
Ian Quinto <codedianq@gmail.com>
2023-09-23 19:41:41 +0800

    Add function of basic computation (+, -, *, /).

---
+++ script.js
@@ -0,0 +1,16 @@
+// Create functions for add, subtract, multiply, and divide.
+function addition(x, y) {
+    return x + y;
+}
+
+function subtraction(x, y) {
+    return x - y;
+}
+
+function multiplication(x, y) {
+    return x * y;
+}
+
+function division(x, y) {
+    return x / y;
+}
