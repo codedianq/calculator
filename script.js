f3c55dfd3fa0b3fd9a4eabbfbb014de7e0accd34
Ian Quinto <codedianq@gmail.com>
2023-09-23 19:58:05 +0800

    Add operate function to calculate the numbers

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//global variables
+let x = 0;
+let y = 0;
+let operator;
+
 // Create functions for add, subtract, multiply, and divide.
 function addition(x, y) {
     return x + y;
@@ -14,3 +19,24 @@ function multiplication(x, y) {
 function division(x, y) {
     return x / y;
 }
+
+// Create a new function named operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function operate(operation, x, y) {
+    if (operation === 'addition') {
+        return addition(x, y);
+    }
+    if (operation === 'subtraction') {
+        return subtraction(x, y);
+    }
+    if (operation === 'multiplication') {
+        return multiplication(x, y);
+    }
+    if (operation === 'division') {
+        return division(x, y);
+    }else {
+        return ('Please choose: addition, subtraction, multiplication, or division')
+    }
+}
+
+console.log(operate('division', 25, 5));
